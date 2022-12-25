d5f3cc07eccc6d394f105fabc6a6c8a45485e0cf
EmirKopuz <121403852+EmirKopuz@users.noreply.github.com>
2022-12-26 01:08:46 +0300

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,69 +1,70 @@
 public class Login extends DbConnection {
 
     static String stuff_username;
-    ManagementManager manager=new ManagementManager();
+    ManagementManager manager=new ManagementManager();     //nesneler oluşturduk
     CustomerManager customerManager=new CustomerManager();
     StuffManager stuffManager =new StuffManager();
 
 
-    static String customer_uniqueId;
+    static String customer_uniqueId;   //başka classlarda nesne oluşturmaya gerek kalmadan kullanmak için
     static int customer_id;
-    public void customerLogin(){
+    public void customerLogin(){   //müşteri girişi
         getCustomers();
         while(true) {
         System.out.print("Müşteri kodunu giriniz : ");
         customer_uniqueId=scanner.next();
 
         for(int i=0;i<customers.size();i++){
-
+            //customer dizisi boyunda döndürüp ıd kontrolu yapıp girilen bilgilari karşılaştırmak
             if(customer_uniqueId.equals(customers.get(i).getCustomer_uniqueID()) ){
                 System.out.println("Giriş Başarılı..\n");
                 System.out.println("Hoş geldin "+ customers.get(i).getCustomer_name());
                 customer_id=i;
-                customerManager.customerMenu();
+                customerManager.customerMenu();// buraya ne yazmak lazım
             }
 
         }
         }
     }
 
-   public void stuffLogin(){
-        getStuff();
+   public void stuffLogin(){     // personel girişi
+        getStuff();    //databaseden personelleri aldık
         while(true) {
             System.out.print("Personel kullanıcı adınızı giriniz : ");
             stuff_username = scanner.next();
             System.out.print("Personel şifrenizi giriniz : ");
             String stuff_password = scanner.next();
-
+            //personale ait giriş bilgilarini aldık
 
             for (int i = 0; i < stuffs.size(); i++) {
-
+                    //müşteride olduğu gibi burdada kontrol edip giriş sağlamak
                 if (stuff_username.equals(stuffs.get(i).getStuff_username()) && stuff_password.equals(stuffs.get(i).getStuff_password())) {
                     System.out.println("Giriş Başarılı\n");
                     System.out.println("Hoş geldiniz " + stuffs.get(i).getStuff_name());
 
-                    updateTime("entrytime", stuff_username);
-                    stuffManager.stuffRedirect(i);
+                    updateTime("entrytime", stuff_username);  //personelimizin giriş saatini databaseye aktardık
+                    stuffManager.stuffRedirect(i); //buraya ne yazmak lazım
                 }
 
             }
 
         }
     }
-    public void managementLogin(){
-        getManagers();
+    public void managementLogin(){  
+        getManagers();  // databasemizden yöneticileri aldık
         while (true) {
             System.out.print("Yönetici kullanıcı adınızı giriniz : ");
             String manager_username = scanner.next();
             System.out.print("Yönetici şifrenizi giriniz : ");
-            String manager_password = scanner.next();
+            String manager_password = scanner.next();     //yöneticinin giriş bilgilerinin alınması
             for (Management management : managers) {
-
-                if (manager_username.equals(management.getManager_username()) && manager_password.equals(management.getManager_password())) {
+            
+                if (manager_username.equals(management.getManager_username()) && manager_password.equals(management.getManager_password())) {  
+                   //yöneticinin giriş bilgilerinin doğruluk karşılaştırması
                     System.out.println("\nGiriş Başarılı\n");
                     System.out.println("Hoş geldiniz " + management.getManager_name());
-
-                    manager.managerMenu();
+                    
+                    manager.managerMenu(); //buraya ne yazmak lazım
 
                 }
 
