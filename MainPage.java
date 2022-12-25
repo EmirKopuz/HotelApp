13e44011e19d757927c5c27c21cec2db9fea900f
EmirKopuz <121403852+EmirKopuz@users.noreply.github.com>
2022-12-26 01:16:05 +0300

    Update MainPage.java

--- MainPage.java
+++ MainPage.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class MainPage {
 
-    public static void showLoginChoice(){
-        Scanner scanner=new Scanner(System.in);
-        Login login = new Login();
+    public static void showLoginChoice(){      //giriş seçenekerini göstermek için 
+        Scanner scanner=new Scanner(System.in); //scanner oluşturdk
+        Login login = new Login();   //login nesnesi oluşturduk
         int giris;
         String kontrol="1";
         System.out.println("Giriş Yap\n********************************");
@@ -12,28 +12,28 @@ public class MainPage {
             System.out.println("1-Müşteri girişi \n2-Personel Girişi\n3-Yönetim Girişi\n4-Çıkış\n**********");
             System.out.print("Seçiminiz : ");
 
-            giris = scanner.nextInt();
+            giris = scanner.nextInt();   // giriş seceneğini alıp else if kullanarak doğru giriş ekranına gönderme
 
             if(giris==1){
                 kontrol="";
 
-                login.customerLogin();
+                login.customerLogin(); //login classımızdan müşteri giriş ekranı 
 
             } else if (giris==2) {
                 kontrol="";
-                login.stuffLogin();
+                login.stuffLogin(); //login classımızdan personel giriş ekranı 
 
 
             }else if(giris==3){
                 kontrol="";
-                login.managementLogin();
+                login.managementLogin();  //login classımızdan yönetici giriş ekranı 
             }else if(giris==4){
 
-                kontrol="0";
+                kontrol="0";  //while döngüsünü kırmak için
 
 
             }else{
-                System.out.println("Hatalı giriş yaptınız !!\n****************");
+                System.out.println("Hatalı giriş yaptınız !!\n****************"); // hatali giriş ihtimali
             }
 
 
